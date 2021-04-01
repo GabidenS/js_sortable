80830dcd8581c5144964ebecf7101ca894d9566c
GS <GS@GSs-iMac.Dlink>
2021-04-02 03:02:16 +0600

    header, menu added

--- script.js
+++ script.js
@@ -15,27 +15,35 @@ const all = fetch(
   });
 
 const addImage = function (objectData) {
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 200; i++) {
     const imageLink = objectData[i].images.md;
     const newCard = document.createElement('img');
     newCard.classList.add('card', `img-${i}`);
     newCard.src = imageLink;
     conatinerElement.appendChild(newCard);
-    document.querySelector(`.img-${i}`).addEventListener('click', function () {
-      const x = document.querySelector(`.img-${i}`).getBoundingClientRect().x;
-      const y = document.querySelector(`.img-${i}`).getBoundingClientRect().y;
-      const width = document.querySelector(`.img-${i}`).getBoundingClientRect()
-        .width;
-      const height = document.querySelector(`.img-${i}`).getBoundingClientRect()
-        .height;
-      console.log(document.querySelector(`.img-${i}`).getBoundingClientRect());
-      let overlay = document.querySelector('.overlay');
+    document
+      .querySelector(`.img-${i}`)
+      .addEventListener('mouseover', function () {
+        const x = document.querySelector(`.img-${i}`).getBoundingClientRect().x;
+        const y = document.querySelector(`.img-${i}`).getBoundingClientRect().y;
+        const width = document
+          .querySelector(`.img-${i}`)
+          .getBoundingClientRect().width;
+        const height = document
+          .querySelector(`.img-${i}`)
+          .getBoundingClientRect().height;
+        console.log(
+          document.querySelector(`.img-${i}`).getBoundingClientRect()
+        );
+        let overlay = document.querySelector('.overlay');
 
-      overlay.style.left = x + 'px';
-      overlay.style.top = y + 'px';
-      overlay.style.height = height + 'px';
-      overlay.style.width = width + 'px';
-    });
+        overlay.style.left = x + 'px';
+        overlay.style.top = y + 'px';
+        overlay.style.height = height + 'px';
+        overlay.style.width = width + 'px';
+
+        overlay.classList.add('overlay--on');
+      });
   }
 };
 
@@ -44,3 +52,9 @@ const addOverlay = function () {
   newDiv.classList.add('overlay');
   document.body.appendChild(newDiv);
 };
+const overlayOff = function () {
+  let overlay = document.querySelector('.overlay');
+  overlay.classList.remove('overlay--on');
+};
+window.addEventListener('resize', overlayOff);
+document.addEventListener('scroll', overlayOff);
