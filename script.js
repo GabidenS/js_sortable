1808ff0433c77e8ba56b61952426ae975855c2ab
GS <GS@GSs-iMac.Dlink>
2021-04-02 23:36:41 +0600

    overlay stays fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 console.log('hello');
 let allDataObject;
 const conatinerElement = document.querySelector('.container');
-
+let activeCard = -1;
 const all = fetch(
   'https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json'
 )
@@ -21,29 +21,24 @@ const addImage = function (objectData) {
     newCard.classList.add('card', `img-${i}`);
     newCard.src = imageLink;
     conatinerElement.appendChild(newCard);
-    document
-      .querySelector(`.img-${i}`)
-      .addEventListener('mouseover', function () {
-        const x = document.querySelector(`.img-${i}`).getBoundingClientRect().x;
-        const y = document.querySelector(`.img-${i}`).getBoundingClientRect().y;
-        const width = document
-          .querySelector(`.img-${i}`)
-          .getBoundingClientRect().width;
-        const height = document
-          .querySelector(`.img-${i}`)
-          .getBoundingClientRect().height;
-        console.log(
-          document.querySelector(`.img-${i}`).getBoundingClientRect()
-        );
-        let overlay = document.querySelector('.overlay');
+    document.querySelector(`.img-${i}`).addEventListener('click', function () {
+      const x = document.querySelector(`.img-${i}`).getBoundingClientRect().x;
+      const y = document.querySelector(`.img-${i}`).getBoundingClientRect().y;
+      activeCard = i;
+      const width = document.querySelector(`.img-${i}`).getBoundingClientRect()
+        .width;
+      const height = document.querySelector(`.img-${i}`).getBoundingClientRect()
+        .height;
+      console.log(document.querySelector(`.img-${i}`).getBoundingClientRect());
+      let overlay = document.querySelector('.overlay');
 
-        overlay.style.left = x + 'px';
-        overlay.style.top = y + 'px';
-        overlay.style.height = height + 'px';
-        overlay.style.width = width + 'px';
+      overlay.style.left = x + 'px';
+      overlay.style.top = y + 'px';
+      overlay.style.height = height + 'px';
+      overlay.style.width = width + 'px';
 
-        overlay.classList.add('overlay--on');
-      });
+      overlay.classList.add('overlay--on');
+    });
   }
 };
 
@@ -56,5 +51,29 @@ const overlayOff = function () {
   let overlay = document.querySelector('.overlay');
   overlay.classList.remove('overlay--on');
 };
-window.addEventListener('resize', overlayOff);
-document.addEventListener('scroll', overlayOff);
+const resetOverlay = function () {
+  if (activeCard != -1) {
+    const x = document
+      .querySelector(`.img-${activeCard}`)
+      .getBoundingClientRect().x;
+    const y = document
+      .querySelector(`.img-${activeCard}`)
+      .getBoundingClientRect().y;
+    const width = document
+      .querySelector(`.img-${activeCard}`)
+      .getBoundingClientRect().width;
+    const height = document
+      .querySelector(`.img-${activeCard}`)
+      .getBoundingClientRect().height;
+    let overlay = document.querySelector('.overlay');
+
+    overlay.style.left = x + 'px';
+    overlay.style.top = y + 'px';
+    overlay.style.height = height + 'px';
+    overlay.style.width = width + 'px';
+
+    overlay.classList.add('overlay--on');
+  }
+};
+window.addEventListener('resize', resetOverlay);
+document.addEventListener('scroll', resetOverlay);
